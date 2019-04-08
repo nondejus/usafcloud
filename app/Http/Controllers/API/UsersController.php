27e958989302d6c0008103bcf8d43bf060ef1fdb
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-04-07 19:09:02 -0600

    WIP - add full path to avatar on api

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -4,42 +4,14 @@ namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Storage;
 
 class UsersController extends Controller
 {
-    public function index()
-    {
-        //
-    }
-
-    public function create()
-    {
-        //
-    }
-
-    public function store()
-    {
-        //
-    }
-
     public function show(Request $request)
     {
-        return $request->user();
-    }
-
-    public function edit()
-    {
-        //
-    }
-
-    public function update()
-    {
-        //
-    }
-
-
-    public function destroy()
-    {
-        //
+        $user = $request->user();
+        $user->avatar = Storage::url($user->avatar);
+        return $user;
     }
 }
