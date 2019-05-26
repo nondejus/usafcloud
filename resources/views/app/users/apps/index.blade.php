b9d99cb87b9578c07e72790180c629aa8475a376
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-05-25 20:04:47 -0600

    wip

--- resources/views/app/users/apps/index.blade.php
+++ resources/views/app/users/apps/index.blade.php
@@ -1,38 +1,55 @@
 @extends('app.users.layouts.layout')
 
-@section('title')
-My Apps
-@endsection
+@section('title', 'My Apps')
 
 @section('user-page-content')
 
 <div class="container my-5">
 
-    @forelse ($user->apps as $app)
+    @if($user->apps)
 
-    <div class="flex p-4 border border-solid rounded">
-        @if ($app->client->avatar)
-        <img class="w-24 mr-3" src="{{ Storage::url($app->client->avatar) }}" alt="{{ $app->client->name }}">
-        @endif
-        <div>
-            <a href="{{ $app->client->homepage_url }}" target="_blank">{{ $app->client->name }}</a>
-            <p class="mb-0">{{ $app->client->description }}</p>
+        <div class="px-2">
+
+            <div class="flex flex-wrap -mx-2">
+
+                @foreach($user->apps as $app)
+
+                    <div class="w-1/3 px-2 border border-solid rounded mx-1 mb-2 hover:bg-grey-lighter flex-1">
+
+                        <div class="flex flex-wrap p-4">
+
+                            @if ($app->client->avatar)
+                                <img class="h-24 mx-auto sm:mx-auto md:mr-2" src="{{ Storage::url($app->client->avatar) }}" alt="{{ $app->client->name }}">
+                            @endif
+
+                            <div class="flex-1">
+                                <a href="{{ $app->client->homepage_url }}" target="_blank">{{ $app->client->name }}</a>
+                                <p class="mb-0">{{ $app->client->description }}</p>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                @endforeach
+
+            </div>
+
         </div>
-    </div>
 
-    @empty
+    @else
 
-    <div class="flex items-center flex-column">
+        <div class="flex items-center flex-column">
 
-        <p class="text-muted text-2xl mb-5 leading-none">
-            You have not authorized any third party apps.
-        </p>
+            <p class="text-muted text-2xl mb-5 leading-none">
+                You have not authorized any third party apps.
+            </p>
 
-        @svg('connect', 'w-1/3 h-auto')
+            @svg('connect', 'w-1/3 h-auto')
 
-    </div>
+        </div>
 
-    @endforelse
+    @endif
 
 </div>
 
