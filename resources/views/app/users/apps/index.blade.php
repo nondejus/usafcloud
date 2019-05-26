709e6d14e0219c8f5d46270afda3b39b273ff30d
Wyatt Castaneda <wyatt.castaneda@gmail.com>
2019-05-25 21:10:46 -0600

    wip

--- resources/views/app/users/apps/index.blade.php
+++ resources/views/app/users/apps/index.blade.php
@@ -4,7 +4,7 @@
 
 @section('user-page-content')
 
-<div class="container tw-my-5">
+<div class="container tw-my-8">
 
     @if($user->apps)
 
@@ -14,17 +14,21 @@
 
                 @foreach($user->apps as $app)
 
-                    <div class="w-1/3 px-2 border border-solid rounded mx-1 mb-2 hover:bg-gray-lighter flex-1">
+                    <div class="tw-w-1/3 tw-px-2 tw-border tw-border-solid tw-rounded tw-mx-1 tw-mb-2 hover:tw-bg-gray-lighter tw-flex-1">
 
-                        <div class="flex flex-wrap p-4">
+                        <div class="tw-flex tw-flex-wrap tw-p-4">
 
                             @if ($app->client->avatar)
-                                <img class="h-24 mx-auto sm:mx-auto md:mr-2" src="{{ Storage::url($app->client->avatar) }}" alt="{{ $app->client->name }}">
+                                <img class="tw-h-24 tw-mx-auto tw-block md:tw-mr-4 md:tw-inline-block" src="{{ Storage::url($app->client->avatar) }}" alt="{{ $app->client->name }}">
                             @endif
 
-                            <div class="flex-1">
-                                <a href="{{ $app->client->homepage_url }}" target="_blank">{{ $app->client->name }}</a>
-                                <p class="mb-0">{{ $app->client->description }}</p>
+                            <div class="md:tw-flex-1">
+                                <a href="{{ $app->client->homepage_url }}" target="_blank">
+                                    {{ $app->client->name }}
+                                </a>
+                                <p class="tw-mb-0">
+                                    {{ $app->client->description }}
+                                </p>
                             </div>
 
                         </div>
@@ -39,13 +43,13 @@
 
     @else
 
-        <div class="flex items-center flex-column">
+        <div class="tw-flex tw-items-center tw-flex-column">
 
-            <p class="text-muted text-2xl mb-5 leading-none">
+            <p class="tw-text-grey-300 tw-text-2xl tw-mb-5 tw-leading-none">
                 You have not authorized any third party apps.
             </p>
 
-            @svg('connect', 'w-1/3 h-auto')
+            @svg('connect', 'tw-w-1/3 tw-h-auto tw-inline-block')
 
         </div>
 
--- resources/views/app/users/layouts/layout.blade.php
+++ resources/views/app/users/layouts/layout.blade.php
@@ -2,50 +2,62 @@
 
 @section('page-content')
 
-<section class="container-fluid tw-bg-gray-200 tw-border-bottom tw-border-solid tw-border-gray-400">
+<section class="container-fluid tw-bg-gray-200 tw-border-gray-300 tw-border-solid tw-border">
 
     <div class="container">
 
-        <div class="tw-flex tw-items-center tw-py-5">
+        <div class="tw-flex tw-items-center tw-py-12">
 
             @if ($user->avatar)
-            <img src="{{ Storage::url($user->avatar) }}" class="w-24 h-24 rounded-full mr-3" alt="{{ $user->name }}">
+
+                <img src="{{ Storage::url($user->avatar) }}" class="tw-w-24 tw-h-24 tw-rounded-full tw-mr-3" alt="{{ $user->name }}">
+
             @endif
+
             <div>
-                <h2 class="text-gray-darkest leading-none mb-2">
+
+                <h2 class="tw-text-gray-900 tw-leading-none tw-mb-2">
                     {{ trim($user->last_name . ', ' . $user->first_name . ' ' . Str::limit($user->middle_name, 1, '')) }}
                 </h2>
-                <p class="text-gray-dark m-0">
-                    <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
-                </p>
+
+                <a href="mailto:{{ $user->email }}" class="tw-block">
+                    {{ $user->email }}
+                </a>
+
             </div>
+
         </div>
 
-        <ul class="nav nav-tabs border-bottom-0">
+        <ul class="nav nav-tabs tw-border-none">
+
             <li class="nav-item">
-                <a class="nav-link text-gray-darkest hover:bg-gray-lightest {{ applyActive('app.users.account.index') }}"
+                <a class="nav-link tw-text-gray-900 hover:tw-bg-gray-100 {{ applyActive('app.users.account.index') }}"
                     href="{{ route('app.users.account.index') }}">
                     Activity
                 </a>
             </li>
+
             <li class="nav-item">
-                <a class="nav-link text-gray-darkest hover:bg-gray-lightest {{ applyActive('app.users.account.apps.index') }}"
+                <a class="nav-link tw-text-gray-900 hover:tw-bg-gray-100 {{ applyActive('app.users.account.apps.index') }}"
                     href="{{ route('app.users.account.apps.index') }}">
                     My Apps
                 </a>
             </li>
+
             <li class="nav-item">
-                <a class="nav-link text-gray-darkest hover:bg-gray-lightest {{ applyActive('app.users.account.teams.index') }}"
+                <a class="nav-link tw-text-gray-900 hover:tw-bg-gray-100 {{ applyActive('app.users.account.teams.index') }}"
                     href="{{ route('app.users.account.teams.index') }}">
                     Teams
                 </a>
             </li>
+
             <li class="nav-item">
-                <a class="nav-link text-gray-darkest hover:bg-gray-lightest {{ applyActive('app.users.account.settings.index') }}"
+                <a class="nav-link tw-text-gray-900 hover:tw-bg-gray-100 {{ applyActive('app.users.account.settings.index') }}"
                     href="{{ route('app.users.account.settings.index') }}">
                     Settings
                 </a>
             </li>
+
         </ul>
 
     </div>
